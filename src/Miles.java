cf1b01a36a235bc4baa409e504113e5436d18186
Neto test <a700aa_78@mail.ru>
2023-01-21 20:38:35 +0300

    initial commit

---
+++ src/Miles.java
@@ -0,0 +1,11 @@
+public class Miles {
+    public static void main(String[] args) {
+
+        int value = 2145;
+        int amount = 20;
+
+        int miles = value / amount;
+        System.out.println("Итого начислено миль " + miles);
+    }
+
+}
